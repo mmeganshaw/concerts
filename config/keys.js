d9170eb50c14d1f257c0e05efcdfb0f9e7742347
Megan Shaw <mmeganshaw@gmail.com>
2018-10-04 23:22:46 -0400

    added mongodb connection

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://brad:brad12@ds143932.mlab.com:43932/megconnector"
+};
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 // initializing app as express
 const app = express();
+
+// bringing in the mongo URI
+const db = require("./config/keys").mongoURI;
+
+// connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 // initial test route
 app.get("/", (req, res) => res.send("Hello World"));
