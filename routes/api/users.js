26829af86ec7ef2d10f258e1fe9516815833cbc7
Megan Shaw <mshaw@datagenomix.com>
2019-02-15 14:44:51 -0500

    adding partial user login logic w/ojwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-// encryping password
+// encrypts password
 const bcrypt = require("bcryptjs");
+// https://www.npmjs.com/package/bcrypt
 
 // bringing in gravatar
 const gravatar = require("gravatar");
@@ -40,7 +41,7 @@ router.post("/register", (req, res) => {
 
       // generate a salt for our password
       bcrypt.genSalt(10, (err, salt) => {
-        // hash the password
+        // hash the password with the salt
         bcrypt.hash(newUser.password, salt, (err, hash) => {
           // if there's an error, give it back
           if (err) throw err;
@@ -55,5 +56,41 @@ router.post("/register", (req, res) => {
     }
   });
 });
+
+// @router Post request to api/users/login
+// @desc Logging in a user / generating a jwt
+// @access Public
+
+router.post("/login", (req, res) => {
+  const password = req.body.password;
+  const email = req.body.email;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "User not found, sorry." });
+    }
+    bcrypt.compare(password, user.password).then(Matched => {
+      if (Matched) {
+        return res.json({ msg: "User is logged in" });
+      } else {
+        return res.status(400).json({ password: "Password is incorrect bud." });
+      }
+    });
+  });
+});
+
+// bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
+
+// });
+
+// create the route tell it to get stuff
+// log password input as const
+// log user input as const
+// search our db for the user
+// if exists, move on to search for pw
+// if does not exist, return that user is not in db
+// compare hashed password with user input password
+// if the same, login
+// if not the same log that not the same
 
 module.exports = router;
